509127f3962c991a264e364ceb577d8fbaf64342
Dean Djermanović <dean74745@gmail.com>
2017-01-12 16:07:18 +0100

    Add ProductContract class

---
+++ app/src/main/java/com/example/dean/inventory/data/ProductContract.java
@@ -0,0 +1,21 @@
+package com.example.dean.inventory.data;
+
+import android.provider.BaseColumns;
+
+public class ProductContract {
+
+    private ProductContract() {
+
+    }
+
+    public static final class ProductEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "products";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PRODUCT_NAME = "name";
+        public static final String COLUMN_PRODUCT_PRICE = "price";
+        public static final String COLUMN_PRODUCT_QUANTITY = "quantity";
+        public static final String COLUMN_PRODUCT_SUPPLIER = "supplier";
+    }
+}
